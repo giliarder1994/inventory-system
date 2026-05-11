bc8139993b0bc3aa443536ff43802a13898f8012
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-11 14:40:56 -0300

    Refactor app.js to improve middleware structure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
-const errorHandler = require('./middlewares/errorHandler'); // Adicionado
+const errorHandler = require('./middlewares/errorHandler'); 
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(errorHandler); // Adicionado como último middleware
+app.use(errorHandler); 
 
 module.exports = app;
