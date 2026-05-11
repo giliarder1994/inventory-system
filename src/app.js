69e74a361d2713fa1f14ea08ce68d3293517ddc1
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-11 14:36:48 -0300

    Add error handler middleware to app
    Added error handler middleware to the Express app.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler'); // Adicionado
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler); // Adicionado como último middleware
 
 module.exports = app;
