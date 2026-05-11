fecad93ca4c439071e3ae9f39ba7224c9e6ffc9d
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-11 14:36:12 -0300

    Add product and supplier routes to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/stock', require('./stockRoutes'));
+router.use('/products', require('./productRoutes')); 
+router.use('/suppliers', require('./supplierRoutes'));
 
 module.exports = router;
