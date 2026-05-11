9856b5862b5f1dcc9ab20a4f6fc83b4166cce444
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-11 14:34:47 -0300

    Add error handler middleware for server errors

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+module.exports = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        error: 'Erro interno no servidor',
+        message: err.message
+    });
+};
