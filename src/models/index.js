6a494987584b36f61a16e703248db42c67ff169d
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2026-05-11 14:35:33 -0300

    Fix relationship between StockMovement and Product
    Corrected the relationship between StockMovement and Product.

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,18 @@
 const sequelize = require('../config/database');
-
 const Product = require('./Product');
 const Supplier = require('./Supplier');
 const StockMovement = require('./StockMovement');
 
-//Relacionamentos
-Supplier.hasMany(Product, {foreignKey: 'supplier_id'});
-Product.belongsTo(Supplier, {foreignKey: 'supplier_id'});
+// Relacionamentos
+Supplier.hasMany(Product, { foreignKey: 'supplier_id' });
+Product.belongsTo(Supplier, { foreignKey: 'supplier_id' });
 
-Product.hasMany(StockMovement, {foreignKey: 'product_id'});
-Product.belongsTo(Product, {foreignKey: 'product_id'});
+Product.hasMany(StockMovement, { foreignKey: 'product_id' });
+StockMovement.belongsTo(Product, { foreignKey: 'product_id' }); // Corrigido aqui
 
 module.exports = {
-  sequelize,
-  Product,
-  Supplier,
-  StockMovement
+    sequelize,
+    Product,
+    Supplier,
+    StockMovement
 };
